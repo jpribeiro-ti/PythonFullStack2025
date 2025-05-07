bb9607e0cdbc251e510b517a55c47f5b1f035e2e
jpribeiro-ti <114706918+jpribeiro-ti@users.noreply.github.com>
2025-05-07 17:13:00 -0300

    Delete UC3 directory

--- UC3/06.verificar_palindromo.py
+++
@@ -1,8 +0,0 @@
-def VerificarPalindromo():
-    texto = input("Digite uma palavra ou frase: ").lower().replace(" ", "")
-    if texto == texto[::-1]:
-        print("É um palíndromo!")
-    else:
-        print("Não é um palíndromo.")
-
-VerificarPalindromo()
