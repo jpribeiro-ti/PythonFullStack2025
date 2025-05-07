3c0c96e273faff1658101b790285b98b28b6e7c7
jpribeiro-ti <114706918+jpribeiro-ti@users.noreply.github.com>
2025-05-07 17:13:54 -0300

    Delete aula-02 directory

--- aula-02/03.entrada-dados.py
+++
@@ -1,9 +0,0 @@
-# Entrada básica
-nome = input("Digite seu nome: ")
-print(f"Bem-vindo, {nome}!")
-
-# Convertendo tipos
-idade = int(input("Digite sua idade: "))
-altura = float(input("Digite sua altura (em metros): "))
-
-print(f"Você tem {idade} anos e {altura}m de altura.")
